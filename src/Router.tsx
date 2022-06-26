a383569a46d1ff019301c431be264e3ad74e9cac
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-06-26 17:51:38 -0300

    Ignite Lab

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Subscribe } from "./pages/Subscribe";
+import { Route, Routes } from "react-router-dom";
+import { Event } from "./pages/Event";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+    </Routes>
+  );
+}
